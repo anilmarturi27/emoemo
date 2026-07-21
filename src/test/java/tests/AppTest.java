9043016d0ba10713aa28627431812989d0fb5f0e
anilmarturi27 <82029438+anilmarturi27@users.noreply.github.com>
2026-07-21 11:52:30 +0530

    hello 4th

--- src/test/java/tests/AppTest.java
+++ src/test/java/tests/AppTest.java
@@ -5,6 +5,7 @@ public class AppTest {
 	public static void main(String[] args) {
 		System.out.println("Hello logsin");
 		System.out.println("GitHub Practice");
+		System.out.println("GitHub2 Practice");
 	}
 	
 }
